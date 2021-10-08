a3c114caef4bb86082db03da8ccf48215cdee67f
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-10-08 15:35:15 -0400

    Code coverage added to 90%

--- projects/uswds-components/src/lib/search/search.component.spec.ts
+++ projects/uswds-components/src/lib/search/search.component.spec.ts
@@ -8,9 +8,9 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [SearchComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +22,87 @@ describe('SearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('onValueChange', () => {
+    it('Should emit an event when the search input value changes', () => {
+      const eventSpy = spyOn(component.onSearchTextChange, 'emit');
+      component.onValueChange('test');
+      fixture.detectChanges()
+      expect(eventSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('focusChange', () => {
+    it('Should emit an event on input blur', () => {
+      const eventSpy = spyOn(component.onBlurChange, 'emit');
+      component.focusChange({ target: { value: 'test' } });
+      fixture.detectChanges()
+      expect(eventSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('onKeydown', () => {
+    it('Should update the model on Enter key', () => {
+      const mockEv = {
+        target: {
+          value: 'test',
+        },
+        code: 'Enter',
+        key: 'Enter',
+        preventDefault: function () { }
+      }
+      const eventSpy = spyOn(component, 'updateModel');
+      component.onKeydown(mockEv);
+      fixture.detectChanges()
+      expect(eventSpy).toHaveBeenCalled();
+      expect(component.model).toBe('test');
+    });
+  });
+  describe('onSubmit', () => {
+    it('Should update the model on when click on the submit', () => {
+      const mockEv = {
+        target: {
+          value: 'test',
+        },
+        code: 'Enter',
+        key: 'Enter',
+        preventDefault: function () { }
+      }
+      component.onSubmit('test', mockEv);
+      fixture.detectChanges()
+      expect(component.model).toBe('test');
+    });
+  });
+  describe('writeValue', () => {
+    it('should implement controlvalueaccessor with default values', () => {
+      component.registerOnChange(_ => undefined);
+      component.registerOnTouched(() => undefined);
+      component.writeValue('aaa');
+      expect(component.model).toBe('aaa');
+    });
+  })
+
+  describe('writeValue', () => {
+    it('should implement controlvalueaccessor with empty values', () => {
+      component.registerOnChange(_ => undefined);
+      component.registerOnTouched(() => undefined);
+      component.writeValue('');
+      expect(component.model).toBe('');
+    });
+  })
+
+  describe('getClass', () => {
+    it('should return class name', () => {
+      component.searchSettings.size = 'big'
+      const cls = component.getClass();
+      expect(cls).toBe('usa-search--big');
+    });
+    it('should return class name', () => {
+      component.searchSettings.size = 'small'
+      const cls = component.getClass();
+      expect(cls).toBe('usa-search--small');
+    });
+
+  })
+
 });
