07b86c5662428d13aff28137ff3c0ec27e39ad4a
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-10-12 10:27:13 -0400

    removed unused variable

--- src/app/search/demos/onchange/search-onchange.component.ts
+++ src/app/search/demos/onchange/search-onchange.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchOnChangeComponent implements OnInit {
   onBlurSearchText = '';
-  OnChangeSearchText = ''
   searchtext = '';
   searchTextModel = '';
 
