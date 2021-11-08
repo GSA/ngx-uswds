22e3a3e087cc280f6ce0938b014c40dadeef97f2
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-11-08 09:42:19 -0500

    Tile option added

--- projects/uswds-formly/src/lib/types/radio.ts
+++ projects/uswds-formly/src/lib/types/radio.ts
@@ -4,7 +4,7 @@ import { FieldType } from '@ngx-formly/core';
 
 @Component({
   template: `
-    <usa-radio-group [formControl]="formControl" [name]="to.name ? to.name : 'radio-group'"  [ariaLabelledBy]="to.ariaLabelledBy">
+    <usa-radio-group [tile]="to.tile" [formControl]="formControl" [name]="to.name ? to.name : 'radio-group'"  [ariaLabelledBy]="to.ariaLabelledBy">
      <ng-container *ngFor="let option of to.options;let i = index">
       <usa-radio [id]="id + '_' + i" [disabled]="option.disabled" [value]="option.value" [checked]="option.checked">{{option.label}}</usa-radio>
      </ng-container>
