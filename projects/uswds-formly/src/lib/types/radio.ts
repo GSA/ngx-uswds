ffe448e10105804572bc00a409779758a37eb792
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-11-29 12:35:47 -0500

    Id reverted

--- projects/uswds-formly/src/lib/types/radio.ts
+++ projects/uswds-formly/src/lib/types/radio.ts
@@ -6,7 +6,7 @@ import { FieldType } from '@ngx-formly/core';
   template: `
     <usa-radio-group [tile]="to.tile" [formControl]="formControl" [name]="to.name ? to.name : 'radio-group'"  [ariaLabelledBy]="to.ariaLabelledBy">
      <ng-container *ngFor="let option of to.options;let i = index">
-      <usa-radio [id]="to.id + '_' + i" [disabled]="option.disabled" [value]="option.value" [checked]="option.checked">{{option.label}}</usa-radio>
+     <usa-radio [id]="id + '_' + i" [disabled]="option.disabled" [value]="option.value" [checked]="option.checked">{{option.label}}</usa-radio>
      </ng-container>
   </usa-radio-group>
   `,
