c8705547487ec8cd368c7bf309ec4090ef3d7639
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2021-12-28 22:39:21 -0500

    Updated demo for dropdown

---
+++ src/app/components/dropdown/demos/dropdown-forms/dropdown-forms.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DropdownOptionsModel } from '@gsa-sam/ngx-uswds';
+
+@Component({
+  selector: 'usa-dropdown-forms',
+  templateUrl: './dropdown-forms.component.html',
+  styles: [
+  ]
+})
+export class DropdownFormsComponent {
+
+  templateModel: any;
+
+  reactiveFormControl = new FormControl();
+
+  dropdownOptions: DropdownOptionsModel[] = [
+    {
+      label: '--Select--',
+      value: null,
+    },
+    {
+      label: 'Option A',
+      value: 'value1'
+    },
+    {
+      label: 'Option B',
+      value: 'value2'
+    },
+    {
+      label: 'Option C',
+      value: 'value3'
+    },
+    {
+      label: 'Option D',
+      value: 'value4'
+    },
+    {
+      label: 'Option E',
+      value: 'value5'
+    }
+  ]
+
+}
--- src/app/components/dropdown/dropdown.module.ts
+++ src/app/components/dropdown/dropdown.module.ts
@@ -5,6 +5,8 @@ import { DemoWrapperComponent } from "../../shared/demo-wrapper.component";
 import { DropdownBasicComponent } from "./demos/basic/dropdown-basic.component";
 import { DropdownBasicModule } from "./demos/basic/dropdown-basic.module";
 import { DocumentationComponentsSharedModule, DocumentationDemoList } from "../../shared";
+import { DropdownFormsModule } from "./demos/dropdown-forms/dropdown-forms.module";
+import { DropdownFormsComponent } from './demos/dropdown-forms/dropdown-forms.component'
 
 
 declare var require: any;
@@ -17,6 +19,14 @@ const DEMOS = {
     markup: require('!!raw-loader!./demos/basic/dropdown-basic.component.html'),
     module: require('!!raw-loader!./demos/basic/dropdown-basic.module'),
     path: 'src/app/components/dropdown/demos/basic',
+  },
+  forms: {
+    title: 'Dropdown with Angular Forms',
+    type: DropdownFormsComponent,
+    code: require('!!raw-loader!./demos/dropdown-forms/dropdown-forms.component'),
+    markup: require('!!raw-loader!./demos/dropdown-forms/dropdown-forms.component.html'),
+    module: require('!!raw-loader!./demos/dropdown-forms/dropdown-forms.module'),
+    path: 'src/app/components/dropdown/demos/dropdown-forms',
   },
 
 }
@@ -46,6 +56,7 @@ export const ROUTES = [
     CommonModule,
     DocumentationComponentsSharedModule,
     DropdownBasicModule,
+    DropdownFormsModule
 
   ],
 })
